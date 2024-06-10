abef8a5e76366ccb0ca6382d232952a0c530bf23
ankitprasad01 <prasadankit224@gmail.com>
2024-06-10 18:45:31 +0530

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+let accountEmail="ankit@123";
+let accountPassword="123";
+let accountCity = "mumbai";
+let accountID = 123;
+console.log(accountID);
+
+console.table([accountEmail,accountPassword,accountCity,accountID]);
