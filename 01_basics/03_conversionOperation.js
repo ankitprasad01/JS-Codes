17d8e97962994102371adf60e156750b3f29acba
ankitprasad01 <prasadankit224@gmail.com>
2024-06-13 14:15:49 +0530

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score='1hdjhdk222';
+console.log(typeof(score));
+let scoreinNumber=Number(score);
+console.log(typeof scoreinNumber);
+console.log(scoreinNumber);
+let isLoggedIn=true;
+console.log(typeof (isLoggedIn));
+let isLoggedIninNumber=Number(isLoggedIn);
