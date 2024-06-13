66709f89485f3e243e45b1bc7b0511bb5da519c5
ankitprasad01 <prasadankit224@gmail.com>
2024-06-13 13:46:41 +0530

    Datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+ "use strict"; //treat all JS code as newer version
+ console.log(3+3);
+ let name="ankit";
+ let age=23;
+ let isLoggedIn=false;
+
+//number => 2 to power 53
+//bigint
+//string=>
+//boolean
+//undefined
+//null
+//symbol
